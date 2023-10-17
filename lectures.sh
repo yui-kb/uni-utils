1d557acbdf77febb4bb9e2a04768153346d4c3dd
Yui <middlebrook.george@gmail.com>
2023-10-17 20:48:00 +0100

    Added editing past lectures

--- lectures.sh
+++ lectures.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-
+# first defining variables which will be used later on (including lists for options/modules)
 parent_dir="/Users/yui/Desktop/notes/second-year/"
 date=$(date +%d-%m-%y)
 template="$parent_dir/_templates/lect.tex"
 lect_file="./lectures/$date.tex"
+
+OPTIONS=("n - New lecture/Edit today's lecture"
+         "e - Edit past lecture"
+         "v - View compiled lectures"
+         "f - Open module folder"
+         "p - Practical")
 
 MODULES=("toc - Theory of Computation"
          "prog - Programming Paradigms"
@@ -11,38 +17,49 @@ MODULES=("toc - Theory of Computation"
          "comp - Complex Analysis II"
          "amv - Analysis in Many Variables II"
          "ent - Elementary Number Theory II"
-         "dssc - Data Science and Statistical Computing")
+         "dssc - Data Science and Statistical Computing"
+         "alg - Algebra II")
+
+option=$(printf '%s\n' "${OPTIONS[@]}" | fzf --preview 'echo {}' --preview-window=up:2:wrap)
+
+case $option in
+         "n - New lecture/Edit today's lecture")
 
 module=$(printf '%s\n' "${MODULES[@]}" | fzf --preview 'echo {}' --preview-window=up:2:wrap)
-
-# there's currently a bug with the following if statement.
-
-if [[ ! -v module ]]; then
-        echo "fuck you"
-        return
-fi
 
 choice="${module%% *}"
 cd $parent_dir$choice
 
 if [[ ! -f ./lectures/$date.tex ]] then
         cp $template $lect_file
-fi
-
-
-#ghead -n -1 ./main.tex > temp ; mv temp ./main.tex
 echo "\subfile{lectures/$date}" >> main.tex
-#echo "\end{document}" >> main.tex
+
 lc=$(wc -l < ./main.tex)
 linea=$lc-1
 lineb=$lc
 {
-    printf '%dm%d\n' "$linea" "$lineb"
-    printf '%d-m%d-\n' "$lineb" "$linea"
+    printf '%dm%d\n' "$lc-1" "$lc"
+    printf '%d-m%d-\n' "$lc" "$lc-1"
     printf '%s\n' w q
 } | ed -s main.tex
-echo $lc
+fi
 vim ./lectures/$date.tex
 # generate a subfile in lectures/ subdirectory
 # add the subfile to the main lecture tex file
 # open the lecture subfile, ready to edit
+;;
+"f - Open module folder")
+module=$(printf '%s\n' "${MODULES[@]}" | fzf --preview 'echo {}' --preview-window=up:2:wrap)
+choice="${module%% *}"
+cd $parent_dir$choice
+;;
+"e - Edit past lecture")
+module=$(printf '%s\n' "${MODULES[@]}" | fzf --preview 'echo {}' --preview-window=up:2:wrap)
+choice="${module%% *}"
+cd $parent_dir$choice/lectures
+lecture=$(find * | fzf --preview 'echo {}' --preview-window=up:2:wrap --tac --no-sort)
+vim ./$lecture
+;;
+*)
+        exit
+esac
